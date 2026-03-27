968380368c7bf98b8ee00208ffe7dc5738fbf9ad
Kirin-KL <klevakin-Kirilll@yandex.ru>
2026-03-27 22:46:15 +0500

    Бот изменён для baresip. прием и ответ на звонок

--- sipbot.py
+++ sipbot.py
@@ -5,16 +5,24 @@ gateway = "181571.voice.plusofon.ru"
 user = "21261774115582"
 pswd = "FlvUenbQ"
 
-class MyVoiceBot(BareSIP):
-    def handle_incoming_call(self, number):
-        print(f"Входящий звонок от {number}")
-        self.accept_call()
 
+class MyVoiceBot(BareSIP):
+    # Входящий вызов
+    def handle_incoming_call(self, number):
+        print(f"Входящий звонок от: {number}")
+        self.accept_call()  # автоответ
+
+    # Вызов установлен
     def handle_call_established(self):
         print("Звонок установлен, говорю...")
         self.speak("Здравствуйте! Вас приветствует голосовой бот.")
         self.speak("Спасибо за звонок. До свидания.")
         self.hang()
+
+    # Вызов завершён
+    def handle_call_ended(self, reason):
+        print(f"Вызов завершён: {reason}")
+
 
 bot = MyVoiceBot(user, pswd, gateway)
 
