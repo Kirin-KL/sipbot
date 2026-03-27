52608bb7eddde5665c982680b1f0ce26558dc23b
Kirin-KL <klevakin-Kirilll@yandex.ru>
2026-03-27 23:17:51 +0500

    Отключаем конфигурацию аккаунта в коде

--- sipbot.py
+++ sipbot.py
@@ -1,30 +1,21 @@
 from baresipy import BareSIP
 from time import sleep
 
-gateway = "181571.voice.plusofon.ru"
-user = "21261774115582"
-pswd = "FlvUenbQ"
-
-
 class MyVoiceBot(BareSIP):
-    # Входящий вызов
     def handle_incoming_call(self, number):
         print(f"Входящий звонок от: {number}")
-        self.accept_call()  # автоответ
+        self.accept_call()
 
-    # Вызов установлен
     def handle_call_established(self):
         print("Звонок установлен, говорю...")
         self.speak("Здравствуйте! Вас приветствует голосовой бот.")
         self.speak("Спасибо за звонок. До свидания.")
         self.hang()
 
-    # Вызов завершён
     def handle_call_ended(self, reason):
         print(f"Вызов завершён: {reason}")
 
-
-bot = MyVoiceBot(user, pswd, gateway)
+bot = MyVoiceBot()  # ВАЖНО: не передавать логин/пароль
 
 while bot.running:
     sleep(1)
