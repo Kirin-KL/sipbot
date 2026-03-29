347d9b3fd6df3b28ae28b968169950a6422512fe
Kirin-KL <klevakin-Kirilll@yandex.ru>
2026-03-29 10:24:02 +0500

    Новый бот на pjsua

--- sipbot.py
+++ sipbot.py
@@ -1,22 +1,70 @@
-from python_sip_client import BareSIP
+import pjsua as pj
+import time
 
-def on_call(from_uri):
-    print("Входящий звонок:", from_uri)
-    bs.answer()
-    bs.play("/path/to/your.wav")   # можно проиграть TTS-файл
-    # или позже: bs.hangup()
-
-bs = BareSIP(path="/usr/bin/baresip", debug=True)
-
-bs.on(BareSIP.Event.INCOMING_CALL, on_call)
-
-bs.create_user_agent(
-    "21261774115582",
-    "FlvUenbQ",
-    "sip:21261774115582@181571.voice.plusofon.ru"
-)
+SIP_DOMAIN = "sip:21261774115582@181571.voice.plusofon.ru"
+SIP_USER = "21261774115582"
+SIP_PASS = "FlvUenbQ"
 
 
-# держим процесс живым
-while True:
-    pass
+def log_cb(level, msg, length):
+    print(msg)
+
+
+lib = pj.Lib()
+
+try:
+    lib.init(log_cfg=pj.LogConfig(level=3, callback=log_cb))
+
+    # для VPS без аудио
+    lib.set_null_snd_dev()
+
+    transport = lib.create_transport(pj.TransportType.UDP)
+    lib.start()
+
+    acc = lib.create_account(pj.AccountConfig(
+        domain=SIP_DOMAIN,
+        username=SIP_USER,
+        password=SIP_PASS
+    ))
+
+    class CallCallback(pj.CallCallback):
+        def __init__(self, call):
+            super().__init__(call)
+
+        def on_state(self):
+            print("Call state:", self.call.info().state_text)
+
+        def on_media_state(self):
+            if self.call.info().media_state == pj.MediaState.ACTIVE:
+                print("📞 Call answered, waiting 10 seconds...")
+
+                time.sleep(10)
+
+                print("❌ Hanging up")
+                self.call.hangup()
+
+    class AccountCallback(pj.AccountCallback):
+        def __init__(self, account):
+            super().__init__(account)
+
+        def on_incoming_call(self, call):
+            print("📞 Incoming call")
+
+            call_cb = CallCallback(call)
+            call.set_callback(call_cb)
+
+            # отвечаем
+            call.answer(200)
+
+    acc.set_callback(AccountCallback(acc))
+
+    print("✅ Ready...")
+
+    while True:
+        time.sleep(1)
+
+except KeyboardInterrupt:
+    print("Exiting...")
+
+finally:
+    lib.destroy()
